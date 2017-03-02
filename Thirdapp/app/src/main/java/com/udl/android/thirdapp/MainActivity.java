d640aee447758405b67fa94947d81dfa4951bdd8
darksoul <sergi0.cach0@hotmail.com>
2017-03-02 21:49:07 +0100

    add pick image

--- Thirdapp/app/src/main/java/com/udl/android/thirdapp/MainActivity.java
+++ Thirdapp/app/src/main/java/com/udl/android/thirdapp/MainActivity.java
@@ -5,14 +5,18 @@ import android.app.Activity;
 import android.app.SearchManager;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.provider.Contacts;
+import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 
@@ -21,6 +25,8 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
     private static final int MYPERMISSIONS_CALL_PHONE = 0;
     private static final int MYPERMISSIONS_SMS = 1;
+    private static final int MYPERMISSIONS_EX_STORAGE = 2 ;
+    private static final int RESULT_LOAD_IMAGE = 0;
     private Intent in;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -120,9 +126,12 @@ public class MainActivity extends Activity implements View.OnClickListener {
                 startActivity(in);
                 break;
             case R.id.button10:
-                in=new Intent(Intent.ACTION_PICK);
-                in.setType("image/*");
-                startActivity(in);
+                if (ContextCompat.checkSelfPermission(this,Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                    accessResources(Manifest.permission.READ_EXTERNAL_STORAGE,MYPERMISSIONS_EX_STORAGE);
+                }else {
+                    in = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    startActivityForResult(in, RESULT_LOAD_IMAGE);
+                }
                 break;
 
         }
@@ -162,13 +171,30 @@ public class MainActivity extends Activity implements View.OnClickListener {
                     startActivity(in);
 
                 } else {
-
+                    in = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    startActivityForResult(in, RESULT_LOAD_IMAGE);
                     // permission denied, boo! Disable the
                     // functionality that depends on this permission.
                 }
                 return;
             }
 
+            case MYPERMISSIONS_EX_STORAGE:{
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                    in = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    startActivityForResult(in, RESULT_LOAD_IMAGE);
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
 
             // other 'case' lines to check for other
             // permissions this app might request
@@ -198,5 +224,19 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
     }
 
-
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = { MediaStore.Images.Media.DATA };
+            Cursor cursor = getContentResolver().query(selectedImage,filePathColumn, null, null, null);
+            cursor.moveToFirst();
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath = cursor.getString(columnIndex);
+            cursor.close();
+            ImageView imageView = (ImageView) findViewById(R.id.imgView);
+            imageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+        }
+    }
 }

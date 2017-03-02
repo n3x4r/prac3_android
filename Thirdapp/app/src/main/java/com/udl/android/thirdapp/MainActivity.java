7f5af9443d0629a1b278e93f769559ab54859e0a
Sergio Cacho <sergi0.cach0@hotmail.com>
2017-03-02 21:51:19 +0100

    ok

--- Thirdapp/app/src/main/java/com/udl/android/thirdapp/MainActivity.java
+++ Thirdapp/app/src/main/java/com/udl/android/thirdapp/MainActivity.java
@@ -9,7 +9,6 @@ import android.net.Uri;
 import android.provider.Contacts;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
